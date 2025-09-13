ad9407ae2bac4e260a965b0293f4c8946442fff2
Daniel da Silva <daniel.da-silva@smake.com>
2025-09-13 20:48:21 +0200

    fix: improved release workflow

--- release.config.js
+++ release.config.js
@@ -4,6 +4,13 @@
 export default {
   plugins: [
     "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/exec",
+      {
+        verifyReleaseCmd:
+          'echo "NEXT_RELEASE_VERSION=${nextRelease.version}" >> "$GITHUB_OUTPUT"',
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@codedependant/semantic-release-docker",
     "@semantic-release/github",
